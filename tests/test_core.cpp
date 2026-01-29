56d0bd4b1f948ffc4d61e00d331cd6b01ae81a5a
Danya Bucenin <danyabucenin@gmail.com>
2026-01-29 11:18:09 +0300

    Added tests for unique IDs and exeption throw

--- tests/test_core.cpp
+++ tests/test_core.cpp
@@ -1,7 +1,12 @@
 // tests/test_core.cpp
 #include <gtest/gtest.h>
+#include <stdexcept>
+#include <set>
+
 #include "task.h"        
 #include "task_manager.h"
+
+
 
 TEST(TaskManagerTest, AddAndGetTask) 
 {
@@ -11,6 +16,7 @@ TEST(TaskManagerTest, AddAndGetTask)
     const auto& task = manager.getTaskById(1);
     EXPECT_EQ(task.getTitle(), "Test");
     EXPECT_EQ(task.getDescription(), "Description");
+    EXPECT_EQ(task.getStatus(), TaskStatus::TO_DO);
 }
 
 TEST(TaskManager, GetTaskCount)
@@ -23,7 +29,30 @@ TEST(TaskManager, GetTaskCount)
     const auto& task_count = manager.getAllTasks().size();
     const auto& tasks = manager.getAllTasks();
     EXPECT_EQ(task_count, 3);
-    EXPECT_EQ(tasks.at(1).getTitle(), "Test1");
-    EXPECT_EQ(tasks.at(3).getTitle(), "Test3");
+}
+
+TEST(TaskManager, GetTaskById_WithInvalidId_ThrowsException)
+{
+    TaskManager manager;
+    EXPECT_THROW(manager.getTaskById(100), std::out_of_range);
+}
+
+TEST(TaskManager, AddMultipleTasks_GeneratesUniqueIds)
+{
+    TaskManager manager;
+    const int num_tasks = 10;
+
+    for(auto i = 0; i < num_tasks; ++i)
+    {
+        manager.addTask("Test", "test");
+    }
+
+    std::set<int> unique_ids;
+    for(const auto& iter : manager.getAllTasks())
+    {
+        unique_ids.insert(iter.first);
+    }
+    EXPECT_EQ(manager.getAllTasks().size(), num_tasks);
+    EXPECT_EQ(unique_ids.size(), num_tasks);
 }
 
